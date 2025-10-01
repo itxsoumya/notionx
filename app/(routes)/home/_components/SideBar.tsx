1a64179e7fb85ff95b5837cb1a96035b9f17d13b
itxsoumya <itxsoumyaranjan@gmail.com>
2025-10-02 00:05:17 +0530

    basic side bar abstract added

---
+++ app/(routes)/home/_components/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className='bg-pink-50 w-72 border-r-2'  >
+      Sidebar
+    </div>
+  )
+}
+
+export default SideBar
---
+++ app/(routes)/home/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from './_components/SideBar'
+
+function HomePage() {
+  return (
+    <div className="flex h-screen">
+      <SideBar/>
+      <div className="">
+        Editor
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
